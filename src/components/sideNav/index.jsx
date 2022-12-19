b0b7008665b45f23f0c02ba29975a43def0b54ba
Exsan Renaldhi <exsan172@gmail.com>
2022-12-19 16:18:27 +0700

    fix side nav

--- src/components/sideNav/index.jsx
+++ src/components/sideNav/index.jsx
@@ -7,7 +7,7 @@ const SideNav = ({ show=false, close, activePgs, move }) => {
         <div className={`${show === true ? "flex" : "hidden"} w-full h-full fixed z-20 justify-end`}>
             <div className="flex bg-black opacity-70 w-full h-full fixed" onClick={() => close(true)}></div>
             <div className="flex w-8/12 bg-white p-5 z-30 items-start flex-col">
-                <div className="flex items-center w-full border-b-2 border-b-gray-500 pb-2">
+                <div className="flex items-center w-full pb-2">
                     <div className="flex w-full">
                         <span>Menu</span>
                     </div>
@@ -16,44 +16,44 @@ const SideNav = ({ show=false, close, activePgs, move }) => {
                     </div>
                 </div>
                 <div className="flex mt-3 flex-col w-full">
-                    <div className={`flex items-center my-3 w-full ${activePgs === "profile" ? "border-b border-b-gray-400 pb-2" : ""}`} onClick={() => move("profile")}>
+                    <div className={`flex items-center my-3 w-full ${activePgs === "profile" ? "text-gray-800" : "text-gray-400"}`} onClick={() => move("profile")}>
                         <div className="flex px-2">
-                            <Icon icon="healthicons:ui-user-profile-outline" fontSize={20}  className="text-gray-500"/>
+                            <Icon icon="healthicons:ui-user-profile-outline" fontSize={20}/>
                         </div>
                         <div className="flex w-full">
-                            <span className="text-gray-500">Profile</span>
+                            <span>Profile</span>
                         </div>
                     </div>
-                    <div className={`flex items-center my-3 w-full ${activePgs === "portofolio" ? "border-b border-b-gray-400 pb-2" : ""}`} onClick={() => move("portofolio")}>
+                    <div className={`flex items-center my-3 w-full ${activePgs === "portofolio" ? "text-gray-800" : "text-gray-400"}`} onClick={() => move("portofolio")}>
                         <div className="flex px-2">
-                            <Icon icon="material-symbols:apk-document-outline" fontSize={20}  className="text-gray-500"/>
+                            <Icon icon="material-symbols:apk-document-outline" fontSize={20}/>
                         </div>
                         <div className="flex w-full">
-                            <span className="text-gray-500">Portofolio</span>
+                            <span>Portofolio</span>
                         </div>
                     </div>
-                    <div className={`flex items-center my-3 w-full ${activePgs === "kemampuan" ? "border-b border-b-gray-400 pb-2" : ""}`} onClick={() => move("kemampuan")}>
+                    <div className={`flex items-center my-3 w-full ${activePgs === "kemampuan" ? "text-gray-800" : "text-gray-400"}`} onClick={() => move("kemampuan")}>
                         <div className="flex px-2">
-                            <Icon icon="game-icons:skills" fontSize={20}  className="text-gray-500"/>
+                            <Icon icon="game-icons:skills" fontSize={20}/>
                         </div>
                         <div className="flex w-full">
-                            <span className="text-gray-500">Kemampuan</span>
+                            <span>Kemampuan</span>
                         </div>
                     </div>
-                    <div className={`flex items-center my-3 w-full ${activePgs === "pengalaman" ? "border-b border-b-gray-400 pb-2" : ""}`} onClick={() => move("pengalaman")}>
+                    <div className={`flex items-center my-3 w-full ${activePgs === "pengalaman" ? "text-gray-800" : "text-gray-400"}`} onClick={() => move("pengalaman")}>
                         <div className="flex px-2">
-                            <Icon icon="mdi:company" fontSize={20}  className="text-gray-500"/>
+                            <Icon icon="mdi:company" fontSize={20}/>
                         </div>
                         <div className="flex w-full">
-                            <span className="text-gray-500">Pengalaman</span>
+                            <span>Pengalaman</span>
                         </div>
                     </div>
-                    <div className={`flex items-center my-3 w-full ${activePgs === "kontak" ? "border-b border-b-gray-400 pb-2" : ""}`} onClick={() => move("kontak")}>
+                    <div className={`flex items-center my-3 w-full ${activePgs === "kontak" ? "text-gray-800" : "text-gray-400"}`} onClick={() => move("kontak")}>
                         <div className="flex px-2">
-                            <Icon icon="material-symbols:perm-contact-calendar-outline-sharp" fontSize={20}  className="text-gray-500"/>
+                            <Icon icon="material-symbols:perm-contact-calendar-outline-sharp" fontSize={20}/>
                         </div>
                         <div className="flex w-full">
-                            <span className="text-gray-500">Kontak</span>
+                            <span>Kontak</span>
                         </div>
                     </div>
                 </div>
